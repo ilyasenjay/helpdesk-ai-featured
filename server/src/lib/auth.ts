aa0f59c16215d93a2643429da4c7c6dfdb87b5d8
ilyass.enjay <ilyass.enjay@gmail.com>
2026-06-24 19:37:07 +0530

    Setup better auth

---
+++ server/src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "./db";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  emailAndPassword: { enabled: true },
+  trustedOrigins: ["http://localhost:5173"],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "AGENT",
+        input: false,
+      },
+    },
+  },
+});
